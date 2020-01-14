d0e01304fca925b252311c40c0d581ccbddaa882
piotr <piotr.lesinski@gmail.com>
2020-01-14 14:06:51 +0100

    fix assign dada source problem

---
+++ src/main/java/lesinski/piotr/dbservice/model/User.java
@@ -0,0 +1,108 @@
+package lesinski.piotr.dbservice.model;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "first_name")
+    @NotNull
+    private String firstName;
+
+    @Column(name = "last_name")
+    @NotNull
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    @NotNull
+    private String email;
+
+    @Column(name = "password")
+    @NotNull
+    private String password;    //TODO add Hash password
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    public User(){
+
+    }
+
+    public User(int id, @NotNull String firstName, @NotNull String lastName, @NotNull String email, @NotNull String password, String createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
+}
